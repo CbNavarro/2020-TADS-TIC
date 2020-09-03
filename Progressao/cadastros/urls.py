822dbfe6cb9c89edd1573f3c685b743a26f28a8d
CbNavarro <lucasnavarro103@gmail.com>
2020-09-03 19:40:58 -0300

    Criação das views e urls para atualização dos registros no banco de dados.

--- Progressao/cadastros/urls.py
+++ Progressao/cadastros/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 # Importa as views que a gente criou 
 from .views import TipoCreate, UsuarioCreate, FuncionarioCreate, CategoriaCreate, ProdutoCreate, VendaCreate
-
+from .views import TipoUpdate, UsuarioUpdate, FuncionarioUpdate, CategoriaUpdate, ProdutoUpdate, VendaUpdate
 # Tem que ser urlpatterns porque é padrão do Django
 urlpatterns = [
     # Todo path tem endereço, sua_view.as_view() e nome
@@ -12,4 +12,11 @@ urlpatterns = [
     path('cadastrar/categoria', CategoriaCreate.as_view(), name='cadastrar-categoria'),
     path('cadastrar/produto', ProdutoCreate.as_view(), name='cadastrar-produto'),
     path('cadastrar/venda', VendaCreate.as_view(), name='cadastrar-venda'),
+
+    path('editar/tipo/<int:pk>/', TipoUpdate.as_view(), name='editar-tipo'),
+    path('editar/usuario/<int:pk>/', UsuarioUpdate.as_view(), name='editar-usuario'),
+    path('editar/funcionario/<int:pk>/', FuncionarioUpdate.as_view(), name='editar-funcionario'),
+    path('editar/categoria/<int:pk>/', CategoriaUpdate.as_view(), name='editar-categoria'),
+    path('editar/produto/<int:pk>/', ProdutoUpdate.as_view(), name='editar-produto'),
+    path('editar/venda/<int:pk>/', VendaUpdate.as_view(), name='editar-venda'),
 ]
--- Progressao/cadastros/views.py
+++ Progressao/cadastros/views.py
@@ -8,6 +8,8 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 
+
+# ==================================== CREATE =============================================
 class TipoCreate(CreateView):
 	model = Tipo
 	fields = ['nome']
@@ -45,3 +47,46 @@ class VendaCreate(CreateView):
 	fields = ['dt_hr_compra', 'usuario', 'funcionario', 'produto']
 	template_name = 'cadastros/formularioCadastro.html'
 	success_url = reverse_lazy('index') 
+
+# ================================================================================================
+# ==================================== UPDATE ====================================================
+
+class TipoUpdate(UpdateView):
+	model = Tipo
+	fields = ['nome']
+	template_name = 'cadastros/formularioCadastro.html'
+	success_url = reverse_lazy('index')
+
+class UsuarioUpdate(UpdateView):
+	model = Usuario
+	fields = ['nome', 'cpf', 'dt_nascimento',
+    'email', 'senha', 'tipo']
+	template_name = 'cadastros/formularioCadastro.html'
+	success_url = reverse_lazy('index')
+
+class FuncionarioUpdate(UpdateView):
+	model = Funcionario
+	fields = ['nome', 'cpf', 'dt_nascimento',
+    'email', 'senha', 'salario', 'dt_hr_admicao', 'dt_hr_demicao', 'tipo']
+	template_name = 'cadastros/formularioCadastro.html'
+	success_url = reverse_lazy('index')
+    
+class CategoriaUpdate(UpdateView):
+	model = Categoria
+	fields = ['nome']
+	template_name = 'cadastros/formularioCadastro.html'
+	success_url = reverse_lazy('index')
+
+class ProdutoUpdate(UpdateView):
+	model = Produto
+	fields = ['nome', 'valor', 'categoria']
+	template_name = 'cadastros/formularioCadastro.html'
+	success_url = reverse_lazy('index')    
+
+class VendaUpdate(UpdateView):
+	model = Venda
+	fields = ['dt_hr_compra', 'usuario', 'funcionario', 'produto']
+	template_name = 'cadastros/formularioCadastro.html'
+	success_url = reverse_lazy('index') 
+
+# ================================================================================================

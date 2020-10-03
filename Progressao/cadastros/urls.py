b582644a58e698dbfea314ab122aa8e1aa336fcc
unknown <lucasnavarro103@gmail.com>
2020-10-03 18:11:10 -0300

    Atividade Bloco 4

--- Progressao/cadastros/urls.py
+++ Progressao/cadastros/urls.py
@@ -3,15 +3,17 @@ from django.urls import path
 # Importa as views que a gente criou 
 from .views import TipoCreate, UsuarioCreate, FuncionarioCreate, CategoriaCreate, ProdutoCreate, VendaCreate
 from .views import TipoUpdate, UsuarioUpdate, FuncionarioUpdate, CategoriaUpdate, ProdutoUpdate, VendaUpdate
+from .views import TipoDelete, UsuarioDelete, FuncionarioDelete, CategoriaDelete, ProdutoDelete, VendaDelete
+from .views import TipoList, UsuarioList, FuncionarioList, CategoriaList, ProdutoList, VendaList
 # Tem que ser urlpatterns porque é padrão do Django
 urlpatterns = [
     # Todo path tem endereço, sua_view.as_view() e nome
-    path('cadastrar/tipo', TipoCreate.as_view(), name='cadastrar-tipo'),
-    path('cadastrar/usuario', UsuarioCreate.as_view(), name='cadastrar-usuario'),
-    path('cadastrar/funcionario', FuncionarioCreate.as_view(), name='cadastrar-funcionario'),
-    path('cadastrar/categoria', CategoriaCreate.as_view(), name='cadastrar-categoria'),
-    path('cadastrar/produto', ProdutoCreate.as_view(), name='cadastrar-produto'),
-    path('cadastrar/venda', VendaCreate.as_view(), name='cadastrar-venda'),
+    path('cadastrar/tipo/', TipoCreate.as_view(), name='cadastrar-tipo'),
+    path('cadastrar/usuario/', UsuarioCreate.as_view(), name='cadastrar-usuario'),
+    path('cadastrar/funcionario/', FuncionarioCreate.as_view(), name='cadastrar-funcionario'),
+    path('cadastrar/categoria/', CategoriaCreate.as_view(), name='cadastrar-categoria'),
+    path('cadastrar/produto/', ProdutoCreate.as_view(), name='cadastrar-produto'),
+    path('cadastrar/venda/', VendaCreate.as_view(), name='cadastrar-venda'),
 
     path('editar/tipo/<int:pk>/', TipoUpdate.as_view(), name='editar-tipo'),
     path('editar/usuario/<int:pk>/', UsuarioUpdate.as_view(), name='editar-usuario'),
@@ -19,4 +21,18 @@ urlpatterns = [
     path('editar/categoria/<int:pk>/', CategoriaUpdate.as_view(), name='editar-categoria'),
     path('editar/produto/<int:pk>/', ProdutoUpdate.as_view(), name='editar-produto'),
     path('editar/venda/<int:pk>/', VendaUpdate.as_view(), name='editar-venda'),
+
+    path('excluir/tipo/<int:pk>/', TipoDelete.as_view(), name='excluir-tipo'),
+    path('excluir/usuario/<int:pk>/', UsuarioDelete.as_view(), name='excluir-usuario'),
+    path('excluir/funcionario/<int:pk>/', FuncionarioDelete.as_view(), name='excluir-funcionario'),
+    path('excluir/categoria/<int:pk>/', CategoriaDelete.as_view(), name='excluir-categoria'),
+    path('excluir/produto/<int:pk>/', ProdutoDelete.as_view(), name='excluir-produto'),
+    path('excluir/venda/<int:pk>/', VendaDelete.as_view(), name='excluir-venda'),
+
+    path('registros/tipo/', TipoList.as_view(), name='registros-tipo'),
+    path('registros/usuario/', UsuarioList.as_view(), name='registros-usuario'),
+    path('registros/funcionario/', FuncionarioList.as_view(), name='registros-funcionario'),
+    path('registros/categoria/', CategoriaList.as_view(), name='registros-categoria'),
+    path('registros/produto/', ProdutoList.as_view(), name='registros-produto'),
+    path('registros/venda/', VendaList.as_view(), name='registros-venda'),
 ]
--- Progressao/cadastros/views.py
+++ Progressao/cadastros/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
 from .models import Tipo, Usuario, Funcionario, Categoria, Produto, Venda
 
@@ -14,39 +15,39 @@ class TipoCreate(CreateView):
 	model = Tipo
 	fields = ['nome']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-tipo')
 
 class UsuarioCreate(CreateView):
 	model = Usuario
 	fields = ['nome', 'cpf', 'dt_nascimento',
     'email', 'senha', 'tipo']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-usuario')
 
 class FuncionarioCreate(CreateView):
 	model = Funcionario
 	fields = ['nome', 'cpf', 'dt_nascimento',
     'email', 'senha', 'salario', 'dt_hr_admicao', 'dt_hr_demicao', 'tipo']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-funcionario')
     
 class CategoriaCreate(CreateView):
 	model = Categoria
 	fields = ['nome']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-categoria')
 
 class ProdutoCreate(CreateView):
 	model = Produto
 	fields = ['nome', 'valor', 'categoria']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')    
+	success_url = reverse_lazy('registros-produto')    
 
 class VendaCreate(CreateView):
 	model = Venda
 	fields = ['dt_hr_compra', 'usuario', 'funcionario', 'produto']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index') 
+	success_url = reverse_lazy('registros-venda') 
 
 # ================================================================================================
 # ==================================== UPDATE ====================================================
@@ -55,38 +56,96 @@ class TipoUpdate(UpdateView):
 	model = Tipo
 	fields = ['nome']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-tipo')
 
 class UsuarioUpdate(UpdateView):
 	model = Usuario
 	fields = ['nome', 'cpf', 'dt_nascimento',
     'email', 'senha', 'tipo']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-usuario')
 
 class FuncionarioUpdate(UpdateView):
 	model = Funcionario
 	fields = ['nome', 'cpf', 'dt_nascimento',
     'email', 'senha', 'salario', 'dt_hr_admicao', 'dt_hr_demicao', 'tipo']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-funcionario')
     
 class CategoriaUpdate(UpdateView):
 	model = Categoria
 	fields = ['nome']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')
+	success_url = reverse_lazy('registros-categoria')
 
 class ProdutoUpdate(UpdateView):
 	model = Produto
 	fields = ['nome', 'valor', 'categoria']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index')    
+	success_url = reverse_lazy('registros-produto')    
 
 class VendaUpdate(UpdateView):
 	model = Venda
 	fields = ['dt_hr_compra', 'usuario', 'funcionario', 'produto']
 	template_name = 'cadastros/formularioCadastro.html'
-	success_url = reverse_lazy('index') 
+	success_url = reverse_lazy('registros-venda') 
 
 # ================================================================================================
+# ==================================== DELETE ====================================================
+
+class TipoDelete(DeleteView):
+	model = Tipo
+	template_name = 'cadastros/formularioCadastro-excluir.html'
+	success_url = reverse_lazy('registros-tipo')
+
+class UsuarioDelete(DeleteView):
+	model = Usuario
+	template_name = 'cadastros/formularioCadastro-excluir.html'
+	success_url = reverse_lazy('registros-usuario')
+
+class FuncionarioDelete(DeleteView):
+	model = Funcionario
+	template_name = 'cadastros/formularioCadastro-excluir.html'
+	success_url = reverse_lazy('registros-funcionario')
+    
+class CategoriaDelete(DeleteView):
+	model = Categoria
+	template_name = 'cadastros/formularioCadastro-excluir.html'
+	success_url = reverse_lazy('registros-categoria')
+
+class ProdutoDelete(DeleteView):
+	model = Produto
+	template_name = 'cadastros/formularioCadastro-excluir.html'
+	success_url = reverse_lazy('registros-produto')    
+
+class VendaDelete(DeleteView):
+	model = Venda
+	template_name = 'cadastros/formularioCadastro-excluir.html'
+	success_url = reverse_lazy('registros-venda') 
+
+# ================================================================================================
+# ==================================== LIST ======================================================
+
+class TipoList(ListView):
+	model = Tipo
+	template_name = 'cadastros/registros/tipo.html'
+
+class UsuarioList(ListView):
+	model = Usuario
+	template_name = 'cadastros/registros/usuario.html'
+
+class FuncionarioList(ListView):
+	model = Funcionario
+	template_name = 'cadastros/registros/funcionario.html'
+    
+class CategoriaList(ListView):
+	model = Categoria
+	template_name = 'cadastros/registros/categoria.html'
+
+class ProdutoList(ListView):
+	model = Produto
+	template_name = 'cadastros/registros/produto.html'    
+
+class VendaList(ListView):
+	model = Venda
+	template_name = 'cadastros/registros/venda.html' 
